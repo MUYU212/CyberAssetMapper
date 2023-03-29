0f43ff8368666b4d04dd4cd783e09174880400f4
Red256 <hbxjyzx@sina.com>
2023-03-29 10:46:10 +0800

    修改信息

---
+++ chage.sh
@@ -0,0 +1,16 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="red256@Red256deMacBook-Pro.local"
+CORRECT_NAME="Red256"
+CORRECT_EMAIL="hbxjyzx@sina.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
