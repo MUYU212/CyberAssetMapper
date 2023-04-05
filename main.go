c37cdde61507948b97df70c0ee57c006c3886ebd
Red256 <hbxjyzx@sina.com>
2023-04-05 15:05:22 +0800

    单元测试模块，和更新了项目目录结构

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"CyberAssetMapper/model"
 	_ "embed"
+	"fmt"
 )
 
 func main() {
 
-	model.InitDB()
-	defer model.CloseDB()
-	//model.Create_taskTable()
-	//model.InsertTask("河北大学测绘项目", "hbu.cn")
-	model.GetTaskByName()
+	//model.InitDB()
+	//defer model.CloseDB()
+	////model.Create_taskTable()
+	////model.InsertTask("河北大学测绘项目", "hbu.cn")
+	//model.GetTaskByName()
+	var name string
+	name = "hello world xxxx"
+	if name != "" {
+		fmt.Println(name)
+	}
 }
